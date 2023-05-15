22b577e8549b318c7b4bbdc17f1b7c3b0cc6703d
Pedro <pedropiacentini@usp.br>
2023-05-15 14:01:26 -0300

    feat: Create customer

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -8,3 +8,22 @@ export async function getCustomers(req, res) {
         res.status(500).send(err.message)
     }
 }
+
+export async function createCustomer(req, res) {
+    const { name, phone, cpf, birthday } = req.body
+
+    const customer = await db.query(`
+        SELECT * FROM customers WHERE cpf = $1
+    `, [cpf])
+
+    if (customer.rows.length !== 0) return res.sendStatus(409)
+    try {
+        await db.query(`
+            INSERT INTO customers (name,phone,cpf,birthday)
+            VALUES ($1, $2, $3, $4)
+        `, [name, phone, cpf, birthday])
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import { getCustomers } from "../controllers/customers.controller.js"
+import { createCustomer, getCustomers } from "../controllers/customers.controller.js"
 import { validateSchema } from "../middlewares/validateSchema.middleware.js"
 import { customerSchema } from "../schemas/customers.schema.js"
 
 const customersRouter = Router()
 
 customersRouter.get("/customers", getCustomers)
+customersRouter.post("/customers", validateSchema(customerSchema), createCustomer)
+
 
 export default customersRouter
