8522c9206d6003e82f4242c51684e1a0bb9d5b9e
Pedro <pedropiacentini@usp.br>
2023-05-15 16:24:44 -0300

    feat: return rental

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -40,3 +40,27 @@ export async function getRentals(req, res) {
         res.status(500).send(err.message)
     }
 }
+
+export async function returnRental(req, res) {
+    const { id } = req.params
+    const date = new Date().toISOString().split("T")[0]
+
+    const rental = await db.query(`
+        SELECT * FROM rentals 
+        WHERE id = $1;
+    `, [id])
+    console.log(rental)
+    if (rental.rows.length == 0) return res.sendStatus(404)
+    if (rental.rows[0].returnDate) return res.sendStatus(400)
+
+    try {
+        await db.query(`
+            UPDATE rentals
+            SET "returnDate" = $1
+            WHERE id = $2
+        `, [date, id])
+        res.sendStatus(200)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { validateSchema } from "../middlewares/validateSchema.middleware.js"
-import { createRental, getRentals } from "../controllers/rentals.controller.js"
+import { createRental, getRentals, returnRental } from "../controllers/rentals.controller.js"
 import { rentalSchema } from "../schemas/rentals.schema.js"
 
 const rentalsRouter = Router()
 
 rentalsRouter.post("/rentals", validateSchema(rentalSchema), createRental)
 rentalsRouter.get("/rentals", getRentals)
+rentalsRouter.post("/rentals/:id/return", returnRental)
 
 export default rentalsRouter
