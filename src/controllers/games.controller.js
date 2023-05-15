562ba8bc54c77371dbf1a588ee6d323fbbc69e60
Pedro <pedropiacentini@usp.br>
2023-05-15 13:38:25 -0300

    fix: Create game

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -12,8 +12,6 @@ export async function getGames(req, res) {
 export async function createGame(req, res) {
     const { name, image, stockTotal, pricePerDay } = req.body
 
-    if (stockTotal <= 0 || pricePerDay <= 0) return res.sendStatus(400)
-
     const game = await db.query(`
         SELECT * FROM games WHERE name = $1
     `, [name])
